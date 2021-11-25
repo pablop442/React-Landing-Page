9c3a1e63747fc29f37045d7df40eb2f4e9354624
CRodr86 <rodrigocfotografia@gmail.com>
2021-11-25 11:39:31 +0000

    Splited card container into two components

--- src/js/component/cardContainer.jsx
+++ src/js/component/cardContainer.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import Card from "./card.jsx";
+import SpecialOffers from "./specialOffers.jsx";
 
-import amsterdam from "../../img/AmsterdamOk.jpg";
-import andorra from "../../img/AndorraOk.jpg";
-import berlin from "../../img/BerlinOk.jpg";
-import bilbao from "../../img/BilbaoOk.jpg";
-import london from "../../img/LondonOk.jpg";
-import malaga from "../../img/MalagaOk.jpg";
-import newDelhi from "../../img/NewDelhiOk.jpg";
-import newYork from "../../img/NewYorkOk.jpg";
-import paris from "../../img/ParisOk.jpg";
-import porto from "../../img/PortoOk.jpg";
-import rome from "../../img/RomeOk.jpg";
-import seville from "../../img/SevilleOk.jpg";
+import BestDestinations from "./bestDestinations.jsx";
 
 const CardContainer = () => {
 	return (
@@ -49,94 +38,8 @@ const CardContainer = () => {
 				</li>
 			</ul>
 			<div className="tab-content" id="pills-tabContent">
-				<div
-					className="tab-pane fade show active"
-					id="pills-home"
-					role="tabpanel"
-					aria-labelledby="pills-home-tab">
-					<div className="row">
-						<Card
-							imgSrc={newYork}
-							imgAlt="New York"
-							title="New York"
-							text="Flight from Madrid"
-							buttonText="390€"></Card>
-						<Card
-							imgSrc={andorra}
-							imgAlt="Andorra"
-							title="Andorra"
-							text="5 days and 3 nights"
-							buttonText="299€"></Card>
-						<Card
-							imgSrc={newDelhi}
-							imgAlt="Taj Mahal"
-							title="New Delhi"
-							text="Flight + 7 days and 5 nights"
-							buttonText="799€"></Card>
-						<Card
-							imgSrc={porto}
-							imgAlt="Porto Portugal"
-							title="Porto"
-							text="5 days and 3 nights"
-							buttonText="249€ "></Card>
-						<Card
-							imgSrc={london}
-							imgAlt="Westminster Palace"
-							title="London"
-							text="4 days and 2 nights"
-							buttonText="199€ "></Card>
-						<Card
-							imgSrc={paris}
-							imgAlt="Eiffel Tower"
-							title="Paris"
-							text="4 days and 2 nights"
-							buttonText="199€ "></Card>
-					</div>
-				</div>
-				<div
-					className="tab-pane fade"
-					id="pills-offers"
-					role="tabpanel"
-					aria-labelledby="pills-offers-tab">
-					<div className="row">
-						<Card
-							imgSrc={amsterdam}
-							imgAlt="Amsterdam"
-							title="Amsterdam"
-							text="3 days and 2 nights"
-							buttonText="99€"></Card>
-						<Card
-							imgSrc={malaga}
-							imgAlt="Malaga"
-							title="Malaga"
-							text="5 days with Manu - All inclusive"
-							buttonText="49€ "></Card>
-						<Card
-							imgSrc={rome}
-							imgAlt="Rome Colosseum"
-							title="Rome"
-							text="3 Nights - Five Stars Hotel"
-							buttonText="249€ "></Card>
-						<Card
-							imgSrc={seville}
-							imgAlt="Seville"
-							title="Seville"
-							text="3 days and 2 nights"
-							buttonText="79€ "></Card>
-						<Card
-							imgSrc={berlin}
-							imgAlt="Brandenburg gate"
-							title="Berlin"
-							text="5 days and 3 nights"
-							buttonText="239€ "></Card>
-						<Card
-							imgSrc={bilbao}
-							imgAlt="Guggenheim Museum"
-							title="Bilbao"
-							text="3 days and 2 nights"
-							buttonText="149€ "></Card>
-					</div>
-				</div>
+				<BestDestinations></BestDestinations>
+				<SpecialOffers></SpecialOffers>
 			</div>
 		</div>
 	);
