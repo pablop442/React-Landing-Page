fcc6e5f5d395228cc422acc43adfc9072afe3a6a
CRodr86 <rodrigocfotografia@gmail.com>
2021-11-25 21:51:48 +0000

    Added style to parallax container

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ import "../styles/carrousel.scss";
 import "../styles/cardContainer.scss";
 import "../styles/navbar.scss";
 import "../styles/blogCard.scss";
-
+import "../styles/parallaxContainer.scss";
 //import your own components
 import Home from "./component/home.jsx";
 
